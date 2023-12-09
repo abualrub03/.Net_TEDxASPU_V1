a65737def2fbebcea34ae31d012d0474350ddbe4
abualrub03 <89502136+abualrub03@users.noreply.github.com>
2023-12-09 15:42:06 +0300

    Add project files.

---
+++ TEDxASPU.Web/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Presentation.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TEDxASPU.Web/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Presentation.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Speakers()
+        {
+            return View();
+        }public IActionResult Schedual()
+        {
+            return View();
+        }
+        public IActionResult Location()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }public IActionResult BlogElements()
+        {
+            return View();
+        }public IActionResult BlogDetails()
+        {
+            return View();
+        }public IActionResult Contact()
+        {
+            return View();
+        }
+        
+    }
+}
